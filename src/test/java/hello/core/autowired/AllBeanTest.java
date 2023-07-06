f220ebe81135ed7378b6c88c83811884483984a9
miiiinju1 <gms07073@ynu.ac.kr>
2023-07-06 12:45:14 +0900

    프로토타입 스코프와 싱글톤 함께 사용 시 문제점을 무식하게 해결하는 방법 1

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -2,6 +2,9 @@ package hello.core.autowired;
 
 import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -10,12 +13,25 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AllBeanTest {
 
     @Test
     void findAllBean() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+
+        Member member = new Member(1L, "userA", Grade.VIP);
+
+        int discountPrice =  discountService.discount(member, 20000, "rateDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(2000);
+
     }
+
 
     static class DiscountService{
         private final Map<String, DiscountPolicy> policyMap;
@@ -30,6 +46,12 @@ public class AllBeanTest {
 
             System.out.println("policyMap = " + policyMap);
             System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String DiscountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(DiscountCode);
+            return discountPolicy.discount(member, price);
+
         }
     }
 
